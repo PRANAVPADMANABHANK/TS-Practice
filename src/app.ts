bb7a0c7f590288adbfe15880217a4e27892e22c4
pranav k <pranavkkeloth@gmail.com>
2023-07-14 12:15:31 +0530

    second commit

--- src/app.ts
+++ src/app.ts
@@ -3,5 +3,14 @@ let age:number = 20
 let states:boolean = true
 let money:undefined
 let life:null = null
+let cat:any = "false"
 
-console.log(firstName, age, states, money, life)
+console.log(firstName, age, states, money, life, cat)
+console.log(typeof(cat))
+
+let Age : number
+Age = 30
+
+console.log(Age)
+
+let Name : number = 10
