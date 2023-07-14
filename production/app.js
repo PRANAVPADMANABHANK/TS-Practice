82568afa1031531424d4c0702d69b8eec13d0d8a
pranav k <pranavkkeloth@gmail.com>
2023-07-14 11:43:18 +0530

    project initialized

---
+++ production/app.js
@@ -0,0 +1,7 @@
+"use strict";
+let firstName = "hari";
+let age = 20;
+let states = true;
+let money;
+let life = null;
+console.log(firstName, age, states, money, life);
---
+++ src/app.ts
@@ -0,0 +1,7 @@
+let firstName:string = "hari"
+let age:number = 20
+let states:boolean = true
+let money:undefined
+let life:null = null
+
+console.log(firstName, age, states, money, life)
